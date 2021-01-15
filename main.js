afb47500c9d7d415945c7fdc62de9854c5663247
vassdeniss <72888249+vassdeniss@users.noreply.github.com>
2021-01-15 20:42:13 +0200

    v1.2 - Adding small features

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-var count = 0;
-// var count = 1000000;
+var count = 2000000;
 var totalCount = 0;
 var clickersCount = 0;
 var ovensCount = 0;
 var chefsCount = 0;
+var tomatoSauce = false;
+var tomatoPercent = 0;
 var clickersPrice = 15;
 var ovensPrice = 100;
 var chefsPrice = 1100;
+var totalCPS = 0;
 window.setInterval(clickersTimer, 1000); 
 window.setInterval(ovensTimer, 1000);
 window.setInterval(chefsTimer, 1000);
@@ -23,12 +25,16 @@ function pizzaAdder() {
     if (count >= 1100) {
         document.getElementById("chefs").style.display = "block";
     }
+    if (count >= 50000) {
+        document.getElementById("tomato").style.display = "block";
+    }
 }
 
 function clickersAdder() {
     var countFixed = count.toFixed(0);
     if (count >= clickersPrice) {
         clickersCount += 1;
+        CPS();
         document.getElementById("clickerCount").innerHTML = "Count: " + clickersCount + ";";
         count = count - clickersPrice;
         document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + countFixed;
@@ -43,6 +49,7 @@ function ovensAdder() {
     var countFixed = count.toFixed(0); 
     if (count >= ovensPrice) {
         ovensCount += 1;
+        CPS();
         document.getElementById("ovensCount").innerHTML = "Count: " + ovensCount + ";";
         count = count - ovensPrice;
         document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + countFixed;
@@ -57,6 +64,7 @@ function chefsAdder() {
     var countFixed = count.toFixed(0); 
     if (count >= chefsPrice) {
         chefsCount += 1;
+        CPS();
         document.getElementById("chefsCount").innerHTML = "Count: " + chefsCount + ";";
         count = count - chefsPrice;
         document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + countFixed;
@@ -64,6 +72,18 @@ function chefsAdder() {
     }
     else {
         alert("You don't have enough pizzas for that!");
+    }
+}
+
+function tomatoAdder() {
+    var countFixed = count.toFixed(0); 
+    if (count >= 1000000) {
+        tomatoSauce = true; 
+        tomatoPercent = (1 / 100) * totalCPS;
+        count = count - 1000000; 
+        document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + countFixed;
+        CPS(); 
+        document.getElementById("tomatoButton").disabled = true;
     }
 }
 
@@ -117,14 +137,37 @@ function showStats() {
     var y = document.getElementById("upgradeMenu");
     if (x.style.display == "none") {
         x.style.display = "block";
-        y.style.display = "none"
+        y.style.display = "none";
     }
     else {
         x.style.display = "none";
-        y.style.display = "block"
+        y.style.display = "block";
+    }
+}
+
+function showPowers() {
+    var x = document.getElementById("powers");
+    var y = document.getElementById("upgradeMenu"); 
+    if (x.style.display == "none") {
+        x.style.display = "block"; 
+        y.style.display = "none";
+    }
+    else {
+        x.style.display = "none"; 
+        y.style.display = "block";
     }
 }
 
 function totalPizzas() {
     document.getElementById("pizzaTotal").innerHTML = "Total Pizzas Clicked: " + totalCount.toFixed(0);
+}
+
+function CPS() {
+    totalCPS = 0.5 * clickersCount + 1 * ovensCount + 8 * chefsCount + tomatoPercent; 
+    document.getElementById("cps").innerHTML = "Clciks Per Second: " + totalCPS;
+}
+
+function changeName() {
+    var name = prompt("Enter restaurant name:");
+    document.getElementById("name").innerHTML = name + "'s Restaurant";
 }
