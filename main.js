9afe2793270049c3a3f0b8c48c5014c3b57954a8
vassdeniss <72888249+vassdeniss@users.noreply.github.com>
2021-01-21 19:08:00 +0200

    Formatted JS Code

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
+// Counters - 5
 var count = 0;
 var totalCount = 0;
-var mouseCPS = 1;
 var clickersCount = 0;
 var ovensCount = 0;
 var chefsCount = 0;
+
+// CPS - 6
+var mouseCPS = 1;
+var clickerCPS = 0;
+var ovensCPS = 0;
+var chefsCPS = 0;
+var totalCPS = 0;
+var tomatoCPS = 0;
+
+// Upgrades Enabled - 2
 var tomatoSauce = false;
-var graduateEnabled = false;
-var gpcMouseAdd = 0;
-var gpcAdd = 0;
-var tomatoPercent = 0;
+var graduateCookers = false;
+
+// Building Prices - 3
 var clickersPrice = 15;
 var ovensPrice = 100;
 var chefsPrice = 1100;
-var totalCPS = 0;
+
+
+// Unlock Checkers - 4
+var graduateEnabled = false;
+var tomatoEnabled = false;
 var ovensEnabled = false;
 var chefsEnabled = false;
-var graduateOptionOn = false;
-var tomatoEnabled = false;
+
 var restaurantName;
-var clickerCPS = 0;
 window.setInterval(clickersTimer, 1000); 
 window.setInterval(ovensTimer, 1000);
 window.setInterval(chefsTimer, 1000);
@@ -58,8 +69,7 @@ function pizzaAdder() {
     count += mouseCPS;
     totalCount += mouseCPS;
     totalPizzas();
-    var countFixed = count.toFixed(0);
-    document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + countFixed;
+    document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + count.toFixed(0);
     if (count >= 100) {
         ovensEnabled = true;
         graduateOptionOn = true;
@@ -78,20 +88,21 @@ function pizzaAdder() {
 
 function formulas() {
     clickerCPS = 0.1 * clickersCount;
-    if (graduateEnabled == true) {
+    if (graduateCookers == true) {
         clickerCPS = clickerCPS * 2;
     }
+    ovensCPS = 1 * ovensCount;
+    chefsCPS = 8 * chefsCount;
 }
 
 function clickersAdder() {
-    var countFixed = count.toFixed(0);
     if (count >= clickersPrice) {
-        clickersCount = clickersCount + 1;
+        clickersCount += 1;
         formulas();
         CPS();
         document.getElementById("clickerCount").innerHTML = "Count: " + clickersCount;
         count = count - clickersPrice;
-        document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + countFixed;
+        document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + count.toFixed(0);
         clickersPriceAdder();
     }
     else { 
@@ -99,14 +110,14 @@ function clickersAdder() {
     }
 }
 
-function ovensAdder() {
-    var countFixed = count.toFixed(0); 
+function ovensAdder() { 
     if (count >= ovensPrice) {
         ovensCount += 1;
+        formulas();
         CPS();
         document.getElementById("ovensCount").innerHTML = "Count: " + ovensCount;
         count = count - ovensPrice;
-        document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + countFixed;
+        document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + count.toFixed(0);
         ovensPriceAdder();
     }
     else {
@@ -118,6 +129,7 @@ function chefsAdder() {
     var countFixed = count.toFixed(0); 
     if (count >= chefsPrice) {
         chefsCount += 1;
+        formulas();
         CPS();
         document.getElementById("chefsCount").innerHTML = "Count: " + chefsCount;
         count = count - chefsPrice;
@@ -129,26 +141,24 @@ function chefsAdder() {
     }
 }
 
-function graduateAdder() {
-    var countFixed = count.toFixed(0); 
+function graduateAdder() { 
     if (count >= 100) {
         graduateEnabled = true; 
         mouseCPS = mouseCPS * 2;
         count = count - 100; 
-        document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + countFixed;
+        document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + count.toFixed(0);
         formulas();
         CPS(); 
         document.getElementById("graduateButton").disabled = true;
     }
 }
 
-function tomatoAdder() {
-    var countFixed = count.toFixed(0); 
+function tomatoAdder() { 
     if (count >= 1000000) {
         tomatoSauce = true; 
-        tomatoPercent = (1 / 100) * totalCPS;
+        tomatoCPS = (1 / 100) * totalCPS;
         count = count - 1000000; 
-        document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + countFixed;
+        document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + count.toFixed(0);
         CPS(); 
         document.getElementById("tomatoButton").disabled = true;
         document.getElementById("TSImage").style.display = "block"; 
@@ -159,24 +169,21 @@ function clickersTimer() {
     count += clickerCPS;
     totalCount += clickerCPS;
     totalPizzas();
-    var countFixed = count.toFixed(0);
-    document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + countFixed;
+    document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + count.toFixed(0);
 }
 
 function ovensTimer() {
-    count += 1 * ovensCount; 
-    totalCount += 1 * ovensCount; 
+    count += ovensCPS 
+    totalCount += ovensCPS
     totalPizzas();
-    var countFixed = count.toFixed(0);
-    document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + countFixed;
+    document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + count.toFixed(0);
 }
 
 function chefsTimer() {
-    count += 8 * chefsCount; 
-    totalCount += 8 * chefsCount;
+    count += chefsCount; 
+    totalCount += chefsCount;
     totalPizzas(); 
-    var countFixed = count.toFixed(0); 
-    document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + countFixed;
+    document.getElementById("pizzaCounter").innerHTML = "Pizzas: " + count.toFixed(0);
 }
 
 function clickersPriceAdder() {
@@ -246,7 +253,7 @@ function totalPizzas() {
 }
 
 function CPS() {
-    totalCPS = clickerCPS + 1 * ovensCount + 8 * chefsCount + tomatoPercent; 
+    totalCPS = clickerCPS + ovensCPS * chefsCPS + tomatoPercent; 
     document.getElementById("cps").innerHTML = "Clicks Per Second: " + totalCPS.toFixed(2);
 }
 
@@ -279,28 +286,35 @@ function blue() {
 }
 
 function save() {
+
+var restaurantName;
     var save = { 
         count: count, 
         totalCount: totalCount, 
-        mouseCPS: mouseCPS,
-        gpcMouseAdd: gpcMouseAdd,
         clickersCount: clickersCount, 
         ovensCount: ovensCount, 
-        chefsCount: chefsCount, 
+        chefsCount: chefsCount,
+
+        mouseCPS: mouseCPS,
+        clickerCPS: clickerCPS,
+        ovensCPS: ovensCPS,
+        chefsCPS: chefsCPS,
+        totalCPS: totalCPS,
+        tomatoCPS: tomatoCPS,
+
         tomatoSauce: tomatoSauce, 
-        toamtoPercent: tomatoPercent, 
+        graduateCookers: graduateCookers,
+
         clickersPrice: clickersPrice, 
         ovensPrice: ovensPrice, 
         chefsPrice: chefsPrice, 
-        totalCPS: totalCPS, 
-        restaurantName: restaurantName,
-        ovensEnabled: ovensEnabled,
-        chefsEnabled: chefsEnabled,
+
         graduateEnabled: graduateEnabled,
         tomatoEnabled: tomatoEnabled,
-        gpcAdd: gpcAdd,
-        graduateOptionOn: graduateOptionOn,
-        clickerCPS: clickerCPS
+        ovensEnabled: ovensEnabled,
+        chefsEnabled: chefsEnabled,
+
+        restaurantName: restaurantName
     }
     localStorage.setItem("save", JSON.stringify(save));
 };
@@ -310,51 +324,61 @@ function load() {
 
     count = load.count;
     totalCount = load.totalCount; 
-    mouseCPS = load.mouseCPS;
-    gpcMouseAdd = load.gpcMouseAdd;
     clickersCount = load.clickersCount;
     ovensCount = load.ovensCount; 
     chefsCount = load.chefsCount; 
+
+    mouseCPS = load.mouseCPS;
+    clickerCPS = load.clickerCPS;
+    ovensCPS = load.ovensCPS;
+    chefsCPS = load.chefsCPS;
+    totalCPS = load.totalCPS;
+    tomatoCPS = load.tomatoCPS;
+
     tomatoSauce = load.tomatoSauce; 
-    tomatoPercent = load.tomatoPercent;
+    graduateCookers = load.graduateCookers;
+
     clickersPrice = load.clickersPrice; 
     ovensPrice = load.ovensPrice;
     chefsPrice = load.chefsPrice;
-    totalCPS = load.totalCPS;
-    restaurantName = load.restaurantName;
-    ovensEnabled = load.ovensEnabled;
-    chefsEnabled = load.chefsEnabled;
+
     graduateEnabled = load.graduateEnabled;
     tomatoEnabled = load.tomatoEnabled;
-    gpcAdd = load.gpcAdd;
-    graduateOptionOn = load.graduateOptionOn;
-    clickerCPS = load.clickerCPS;
+    ovensEnabled = load.ovensEnabled;
+    chefsEnabled = load.chefsEnabled;
+
+    restaurantName = load.restaurantName;
 
     document.getElementById("name").innerHTML = restaurantName + "'s Restaurant";
     document.getElementById("cps").innerHTML = "Clicks Per Second: " + totalCPS.toFixed(2);
+
     if (ovensEnabled == true) {
         document.getElementById("ovens").style.display = "";
     }
     if (chefsEnabled == true) {
         document.getElementById("chefs").style.display = "";
     }
-    if (graduateOptionOn == true) {
+    if (graduateEnabled == true) {
         document.getElementById("GPC").style.display = "";
     }
     if (tomatoEnabled == true) {
         document.getElementById("tomato").style.display = "";
     }
+
     document.getElementById("clickerCount").innerHTML = "Count: " + clickersCount;
     document.getElementById("ovensCount").innerHTML = "Count: " + ovensCount;
     document.getElementById("chefsCount").innerHTML = "Count: " + chefsCount;
+
     document.getElementById("clickersPrice").innerHTML = "Price: " + clickersPrice.toFixed(0);
     document.getElementById("ovensPrice").innerHTML = "Price: " + ovensPrice.toFixed(0);
     document.getElementById("chefsPrice").innerHTML = "Price: " + chefsPrice.toFixed(0);
+
+    if (graduateCookers == true) {
+        document.getElementById("graduateButton").disabled = true;
+    }
+
     if (tomatoSauce == true) {
         document.getElementById("tomatoButton").disabled = true;
         document.getElementById("TSImage").style.display = "block"; 
-    }
-    if (graduateEnabled == true) {
-        document.getElementById("graduateButton").disabled = true;
     }
 }
